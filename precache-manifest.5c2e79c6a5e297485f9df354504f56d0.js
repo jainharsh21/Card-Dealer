38e5a80cb8f3b2742ce66fc203e7d0aedc8978ca
HARSH JAIN <jainharsh270@gmail.com>
2020-04-23 17:20:33 +0530

    Updates

---
+++ precache-manifest.5c2e79c6a5e297485f9df354504f56d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a78dca6678a43ff45832bca295fb88",
+    "url": "/Card-Dealer/index.html"
+  },
+  {
+    "revision": "b78aea1b13f9f967ecc8",
+    "url": "/Card-Dealer/static/css/main.2285ac5b.chunk.css"
+  },
+  {
+    "revision": "2323b75d733f3a3a9a85",
+    "url": "/Card-Dealer/static/js/2.83256e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Card-Dealer/static/js/2.83256e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78aea1b13f9f967ecc8",
+    "url": "/Card-Dealer/static/js/main.bf262471.chunk.js"
+  },
+  {
+    "revision": "4923a439262180266afb",
+    "url": "/Card-Dealer/static/js/runtime-main.45ce0470.js"
+  }
+]);
